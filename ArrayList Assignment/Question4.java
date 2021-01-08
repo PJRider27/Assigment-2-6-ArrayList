a1683b1747c58b267c3f3f5ce958086e54d53258
PJRider27 <55715739+PJRider27@users.noreply.github.com>
2021-01-08 13:04:04 -0800

    Add files via upload

---
+++ ArrayList Assignment/Question4.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+class Question4 {
+    
+    public static void filter(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        //This method removes all the elements in list1 that also appear in list2
+        for(int i=0;i<list2.size();i++){
+            for(int j=list1.size()-1;j>0;j--){
+                if(list1.get(j).equals(list2.get(i))){
+                    list1.remove(j);
+                    break;
+                }
+            }
+        }
+    }
+    public static void main (String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+        list2.add(2);
+        list2.add(4);
+        System.out.println("The list before: " + list1);
+        filter(list1,list2);
+        System.out.println("The list after:  " + list1);
+        //the list after should be [1,3]
+    }
+    
+    
+    
+}
